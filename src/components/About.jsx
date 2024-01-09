d6e765c7d51f168875dbefb01125905d63fe4de8
Phillip Lu <Plu2978@conestogac.on.ca>
2024-01-08 21:53:32 -0500

    editing about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,7 +17,7 @@ export default function About() {
                     />
                     <div className={`${currentTheme.text}`}>
                         <p>
-                            Hi! Welcome to my resume website made using React. I'm a full-stack developer with experience in various languages and technologies. I also have experience in research and data analysis using Microsoft Excel and Power BI. Flexible and willing to take other positions that may better fit my qualifications.
+                            Hi! Welcome to my resume website made using React. I'm a full-stack developer with 1 year of work experience. I also have 3 years of research and data analysis experience. Flexible and willing to take other positions that may better fit my qualifications.
                         </p>
                     </div>
                 </div>
